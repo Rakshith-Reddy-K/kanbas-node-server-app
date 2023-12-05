b3462333de95d9286686e590ad4dbfacc5adb12b
rakshithreddy <kokonda.r@northeastern.edu>
2023-12-04 23:02:44 -0500

    cors

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ import "dotenv/config";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-}
-));
+app.use(cors());
 const sessionOptions = {
     secret: "any string",
     resave: false,
